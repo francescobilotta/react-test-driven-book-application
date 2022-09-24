ff31fbcd0ae03f472a2c7cdc5a7ce4aa5ee4c7ee
Bilotta Francesco <business@bilottafrancesco.com>
2022-09-24 02:06:21 -0700

    Formatted app.tsx and changed to hello world

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { Typography } from "@mui/material";
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typography component="h2" data-test="heading" variant="h2">
+        Hello world
+      </Typography>
     </div>
   );
 }
